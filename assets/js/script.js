a1d35b52ac2d89cdaa0bcc5c8b47653e006fc150
merzann <annisch78@gmail.com>
2024-06-29 20:13:12 +0000

    maint: add comments throughout script.js detailling structure and elemnts used

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+/**
+ * DOM EventListener
+ * global variables for game sections
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById('game-setup');
     const resetButton = document.getElementById("reset-button");
@@ -6,7 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
     gameBoard.id = 'gameBoard';
     gameSection.appendChild(gameBoard);
 
-    // Music elements
+    /**
+     *  Declare variables for Music and Volume
+     *  Set music to off by default
+    */
     const backgroundMusic = document.getElementById('background-music');
     const gameMusic = document.getElementById('game-music');
     const volumeControl = document.getElementById('volume-control');
@@ -17,11 +24,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let isMusicPlaying = false;
 
+    /* EventListener to prevent game from starting when user input incomplete */
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         startGame();
     });
 
+    /* Control which music is played based on game state */
     volumeControl.addEventListener('click', () => {
         if (isMusicPlaying) {
             backgroundMusic.pause();
@@ -29,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
             volumeIcon.classList.remove('fa-volume-up');
             volumeIcon.classList.add('fa-volume-xmark');
         } else {
-            // Resume the appropriate music based on game state
+            /* Resume the appropriate music based on game state */
             if (gameBoard.innerHTML === '') {
                 backgroundMusic.play();
             } else {
@@ -41,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
         isMusicPlaying = !isMusicPlaying;
     });
 
+    /* Create cards */
     let cardsArray = [];
 
     function startGame() {
@@ -50,20 +60,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
         cardsArray = generateCardsArray(numberOfCards / 2);
 
-        // Clear previous game board
-        // Set board class based on the number of cards
+        /**
+         *  Clear previous game board upon reset button clicked
+         *  Set board class based on the number of cards
+        */
         gameBoard.innerHTML = '';
         gameBoard.className = `board-${numberOfCards}`;
 
         createBoard(cardsArray, cardColor);
 
-        // Stop background music and play game music
+        /* Stop background music and play game music */
         backgroundMusic.pause();
         if (isMusicPlaying) {
             currentGameMusic.play();
         }
     }
 
+    /**
+     *  Generate cards array with 12 name:image object pairs 
+     *  Randomize card selection for better UX
+     * */
     function generateCardsArray(pairCount) {
         let baseCards = [
             { name: 'bracelet    pronunciation: breɪslət', img: 'image1' },
@@ -84,6 +100,10 @@ document.addEventListener("DOMContentLoaded", () => {
         return selectedCards.concat(selectedCards).sort(() => 0.5 - Math.random());
     }
 
+    /**
+     * Create gameBoard by appending name and img based on number of cards selected
+     * EventListener for flipCard
+     */
     function createBoard(cardsArray, cardColor) {
         cardsArray.forEach((item, index) => {
             let card = document.createElement('div');
@@ -106,6 +126,11 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    /**
+     * Define variables for checking flipped cards for match
+     * Pass value of cards chosen to checkForMatch function
+     * Boolean condition lockBoard to prevent user from flipping more than 2 cards at the same time
+     */
     let cardsChosen = [];
     let cardsChosenId = [];
     let cardsWon = [];
@@ -123,6 +148,10 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    /**
+     * Check cards flipped for match, continue game based on outcome
+     * Show message to user when cards match containing vocabulary and pronunciation
+     */
     function checkForMatch() {
         let cards = document.querySelectorAll('.card');
         let optionOneId = cardsChosenId[0];
@@ -145,6 +174,10 @@ document.addEventListener("DOMContentLoaded", () => {
             cards[optionTwoId].classList.remove('flipped');
         }
 
+        /**
+         * Unlock board when no match
+         * final message when all cards have been flipped
+        */
         cardsChosen = [];
         cardsChosenId = [];
         lockBoard = false;
@@ -154,6 +187,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    /* Function and EventListener for game reset */
     function resetGame() {
         cardsChosen = [];
         cardsChosenId = [];
@@ -163,7 +197,7 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset();
         firstMatch = true;
 
-        // Stop current game music and select a new random game music
+        /* Stop current game music and select a new random game music */
         gameMusicArray.forEach(music => music.pause());
         currentGameMusic = gameMusicArray[Math.floor(Math.random() * gameMusicArray.length)];
 
@@ -174,6 +208,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     resetButton.addEventListener('click', resetGame);
 
-    // Start a new game
+    /* Start a new game */
     startGame();
 });
